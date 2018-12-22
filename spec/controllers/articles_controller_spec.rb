24d2e8ad0a499bbb7eff61265b9b9e1325f6a0a9
La Fountain <github email address>
2018-12-22 04:01:22 -0500

    add serializer and completed test for json data return

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -8,13 +8,25 @@ describe ArticlesController do
       get :index
       expect(response).to have_http_status(:ok)
     end
+
     #Make sure test have a body so (render json:{} wont work alone)
     it 'should return proper json' do
+      create_list :article, 2
       get :index
       json = JSON.parse(response.body)
-      pp json
-      # json_data = json[:data]
-      # expect(json_data.length).to eq(2)
+      # pp json
+      json_data = json["data"]
+      expect(json_data.length).to eq(2)
+      expect(json_data[0]['attrributes']).to eq({
+        "title" => "My awesome article 1",
+        "content" => "The content of my awesome article 1",
+        "slug" => "my-awesome-article-1"
+        })
+        expect(json_data[1]['attrributes']).to eq({
+        "title" => "My awesome article 2",
+        "content" => "The content of my awesome article 2",
+        "slug" => "my-awesome-article-2"
+        })
     end
 
   end
